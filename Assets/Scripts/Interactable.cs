88a419c1f543f5f3cdbc00647a3a9dc0bf0c80e6
FaraazSuffla <faraazsuffla29@gmail.com>
2024-05-27 21:48:14 +0200

    Scene Change
    Added Scene changing option to unity event

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,17 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UIElements.Experimental;
 
 public class Interactable : MonoBehaviour
 {
     Outline outline;
     public string messaage;
-
     public UnityEvent onInteraction;
-    
-    // Start is called before the first frame update
+    public string sceneToLoad; // Name of the scene to load
+
     void Start()
     {
         outline = GetComponent<Outline>();
@@ -20,7 +16,8 @@ public class Interactable : MonoBehaviour
 
     public void Interact()
     {
-            onInteraction.Invoke();
+        // Invoke other interaction events
+        onInteraction.Invoke();
     }
 
     public void DisableOutline()
@@ -31,5 +28,28 @@ public class Interactable : MonoBehaviour
     public void EnableOutline()
     {
         outline.enabled = true;
+    }
+
+    // Custom function to change scene
+    public void ChangeScene()
+    {
+        // Check if the scene to load is not empty
+        if (!string.IsNullOrEmpty(sceneToLoad))
+        {
+            // Check if the scene exists in the build settings
+            if (Application.CanStreamedLevelBeLoaded(sceneToLoad))
+            {
+                // Load the scene
+                UnityEngine.SceneManagement.SceneManager.LoadScene(sceneToLoad);
+            }
+            else
+            {
+                Debug.LogError("Scene " + sceneToLoad + " does not exist in the build settings.");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("No scene specified to load.");
+        }
     }
 }
